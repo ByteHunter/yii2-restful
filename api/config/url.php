cbcda522801d2647bd85c635c40a6be9cb9f2c1f
ByteHunter <info@bytehunter.net>
2018-05-10 21:14:56 +0200

    Options verb for resource routes suffixed with `/`

--- api/config/url.php
+++ api/config/url.php
@@ -7,7 +7,16 @@ return [
             'OPTIONS login' => 'options',
         ],
     ],
+    [
+        'class' => 'yii\rest\UrlRule',
+        'pluralize' => false,
+        'only' => ['options'],
+        'suffix' => '/',
+        'controller' => [
+            'v1/user',
+        ],
+    ],
     'status' => 'site/status',
-    'GET verify/token' => 'site/verify-token',
-    'OPTIONS verify/token' => 'site/options',
+    'GET v1/verify/token' => 'site/verify-token',
+    'OPTIONS v1/verify/token' => 'site/options',
 ];
